19a88ed25014225850da646aa9c62452a4e9d15d
bexandy <bexandy@gmail.com>
2015-08-23 11:55:57 -0430

    modificaciones visuales

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'nin1310805495463');
+define('DB_NAME', 'ninacleaning');
 
 /** MySQL database username */
-define('DB_USER', 'nin1310805495463');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'G4b72015@');
+define('DB_PASSWORD', 'p3lk4x');
 
 /** MySQL hostname */
-define('DB_HOST', 'nin1310805495463.db.7964284.hostedresource.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
